97ee4a905177b6df71c02e002c1fd7b980696230
supernova789 <50611789+supernova789@users.noreply.github.com>
2020-06-01 11:49:03 +0530

    Add files via upload

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "TerraformEC2Instance" {
   ami           = "ami-09d95fab7fff3776c"
   instance_type = "t2.micro"
-  iam_instance_profile = "${aws_iam_instance_profile.ec2_profile}"
+  iam_instance_profile = "${aws_iam_instance_profile.ec2_profile.name}"
   tags = {
     Name = "TerraformEC2"
   }
