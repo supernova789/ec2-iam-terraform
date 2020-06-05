2d8f08d11db65ff002417a330c9bb15929f5059b
supernova789 <50611789+supernova789@users.noreply.github.com>
2020-06-05 11:45:29 +0530

    Add files via upload

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role_policy" "ec2_policy" {
   name = "ec2_policy"
-  role = aws_iam_role.terraform_ec2_role.id
+  role = "${aws_iam_role.terraform_ec2_role.id}"
 
   policy = "${file("ec2-policy.json")}"
 }
@@ -11,7 +11,7 @@ resource "aws_iam_role" "terraform_ec2_role" {
   assume_role_policy = "${file("ec2-assume-policy.json")}"
 }
 
-resource "aws_iam_instance_profile" "ec2_profile" {
+resource "aws_iam_instance_profile" "ec2_profile2" {
   name = "terraform_ec2_profile"
   role = "${aws_iam_role.terraform_ec2_role.name}"
 }
